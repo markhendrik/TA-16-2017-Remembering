a30d52183425de2444eadbe94b31efcba6d79c9f
Mark <mark.maeste@gmail.com>
2017-10-11 10:01:27 +0300

    Math.random() example added.

--- src/nameGeneration/StartGenerator.java
+++ src/nameGeneration/StartGenerator.java
@@ -10,7 +10,9 @@ public class StartGenerator {
 		
 		//Random demo
 		// Math.random() vs Random klass
-		System.out.println("juhuslik ujuvkomaga arv 0-1; " + Math.random());		
+		System.out.println("juhuslik ujuvkomaga arv 0-1; " + Math.random());
+		System.out.println("juhuslik arv 100 piires: " + (int) (Math.random() * 101));
+		
 		System.out.println("juhuslik arv Random klassiga " + new Random().nextInt());
 		System.out.println("juhuslik arv Random klassiga, piiritletud  " + new Random().nextInt(10));
 	}
