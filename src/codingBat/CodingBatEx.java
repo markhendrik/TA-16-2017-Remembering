e162a54009c90e7568312defbb5c0270540190c1
Mark <mark.maeste@gmail.com>
2017-11-15 14:38:13 +0200

    getLessThan5 added

--- src/codingBat/CodingBatEx.java
+++ src/codingBat/CodingBatEx.java
@@ -8,57 +8,58 @@ public class CodingBatEx {
 
 	public static void main(String[] args) {
 		CodingBatEx cbe = new CodingBatEx();
-		
-		String[] stringArray = {"tahvel", "auk", "klass", "ahven"};
-		int[] intArray = {5, 6, 45, 99, 13, 5, 6};
+
+		String[] stringArray = { "tahvel", "auk", "klass", "ahven" };
+		int[] intArray = { 5, 6, 45, 99, 13, 5, 6 };
 		List<Integer> listInts = new ArrayList<>();
 		listInts.add(5);
-		listInts.add(6);
+		listInts.add(2);
 		listInts.add(45);
-		listInts.add(99);
-		
+		listInts.add(3);
+
 		System.out.println("Massiivi pikkus " + intArray.length);
 		System.out.println("Listi pikkust " + listInts.size());
-		
-		//massiivi lisamine
+
+		// massiivi lisamine
 		intArray[0] = 17;
-		System.out.println(Arrays.toString(intArray));
-		
-		//elemendi v‰‰rtuse saamine
+		System.out.println("Massiiv peale muutmist " + Arrays.toString(intArray));
+
+		// elemendi v‰‰rtuse saamine
 		System.out.println("Esimene element masiivis: " + intArray[0]);
 		System.out.println("Esimene element listis: " + listInts.get(0));
+
+		/* Sublist
 		
-		//Sublist
 		System.out.println("Sublisti elemendid " + listInts.subList(0, 2));
 		listInts.subList(0, 2).clear();
 		System.out.println("List peale eemaldamist " + listInts);
+		*/
 		
-		
-		//System.out.println(Arrays.toString(cbe.subArray(intArray, 1, 3)));
-		
-		
-		//System.out.println(cbe.sameEnds(intArray, 2));
-		System.out.println("countA: " + cbe.countA(stringArray));
-		
+		// System.out.println(Arrays.toString(cbe.subArray(intArray, 1, 3)));
+
+		// System.out.println(cbe.sameEnds(intArray, 2));
+		//System.out.println("countA: " + cbe.countA(stringArray));
+		System.out.println("getLessThan5: " + cbe.getLessThan5(listInts));
+
 	}
-	
+
 	public boolean sameEnds(int[] nums, int len) {
-		/* version 1
-		int[] beg = subArray(nums, 0, len);
-		int[] end = subArray(nums, nums.length - len, nums.length);	
-		return Arrays.equals(beg, end); */
-		
+		/*
+		 * version 1 int[] beg = subArray(nums, 0, len); int[] end = subArray(nums,
+		 * nums.length - len, nums.length); return Arrays.equals(beg, end);
+		 */
+
 		// version 2
 		for (int i = 0; i < len; i++) {
-//			System.out.println("Algus " + nums[i]);
-//			System.out.println("Lıpust " + nums[nums.length-len+i]);
-			if (nums[i] != nums[nums.length-len+i]) {
+			// System.out.println("Algus " + nums[i]);
+			// System.out.println("Lıpust " + nums[nums.length-len+i]);
+			if (nums[i] != nums[nums.length - len + i]) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	private int[] subArray(int[] array, int beg, int end) {
 		int[] result = new int[end - beg];
 		int index = 0;
@@ -68,27 +69,36 @@ public class CodingBatEx {
 		}
 		return result;
 	}
-	
+
 	// Etteantud massiivist loetleda kokku mitu sına algab ìaî t‰hega.
-	//countA(["aken", "uks", "kollane"] => 1
+	// countA(["aken", "uks", "kollane"] => 1
 	public int countA(String[] strings) {
-		/* version 1
+		/*
+		 * version 1 int count = 0; for (String str : strings) { if(str.substring(0,
+		 * 1).equals("a")) { count++; } } return count;
+		 */
+
+		// version 2
 		int count = 0;
-		for (String str : strings) {
-			if(str.substring(0, 1).equals("a")) {
+		for (int i = 0; i < strings.length; i++) {
+			if (strings[i].startsWith("a")) {
 				count++;
 			}
 		}
-		return count; */
-		
-		//version 2
-		int count = 0;
-		for (int i = 0; i < strings.length; i++) {			
-			if(strings[i].startsWith("a")) {
-				count++;
-			}
-		}
-		return count;	
+		return count;
 	}
 
+	
+	// Listist, esialgset listi muutmata, vıtta v‰lja kıik 5 v‰iksemad arvud ning
+	// tagastada list (vıi massiiv) nendest.
+	// getLessThan5([1, 5, 4, 7] => [1, 4]
+	public List<Integer> getLessThan5(List<Integer> list) {
+		List<Integer> result = new ArrayList<>();
+		for (Integer item : list) {
+			if (item < 5) {
+				result.add(item);
+			}
+		}
+		return result;
+	}
 }
