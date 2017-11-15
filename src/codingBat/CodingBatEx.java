a56e92e86e4f532372c3c18acb5c5cdb89fa2fa4
Mark <mark.maeste@gmail.com>
2017-11-15 13:52:43 +0200

    List and array demos added.

--- src/codingBat/CodingBatEx.java
+++ src/codingBat/CodingBatEx.java
@@ -1,6 +1,8 @@
 package codingBat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CodingBatEx {
 
@@ -8,19 +10,51 @@ public class CodingBatEx {
 		CodingBatEx cbe = new CodingBatEx();
 		
 		int[] intArray = {5, 6, 45, 99, 13, 5, 6};
+		List<Integer> listInts = new ArrayList<>();
+		listInts.add(5);
+		listInts.add(6);
+		listInts.add(45);
+		listInts.add(99);
+		
+		System.out.println("Massiivi pikkus " + intArray.length);
+		System.out.println("Listi pikkust " + listInts.size());
+		
+		//massiivi lisamine
+		intArray[0] = 17;
+		System.out.println(Arrays.toString(intArray));
+		
+		//elemendi v‰‰rtuse saamine
+		System.out.println("Esimene element masiivis: " + intArray[0]);
+		System.out.println("Esimene element listis: " + listInts.get(0));
+		
+		//Sublist
+		System.out.println("Sublisti elemendid " + listInts.subList(0, 2));
+		listInts.subList(0, 2).clear();
+		System.out.println("List peale eemaldamist " + listInts);
+		
+		
 		//System.out.println(Arrays.toString(cbe.subArray(intArray, 1, 3)));
+		
 		
 		System.out.println(cbe.sameEnds(intArray, 2));
 		
 	}
 	
 	public boolean sameEnds(int[] nums, int len) {
+		/* version 1
 		int[] beg = subArray(nums, 0, len);
-		int[] end = subArray(nums, nums.length - len, nums.length);
-		System.out.println("Algus " + Arrays.toString(beg));
-		System.out.println("Lıpp " + Arrays.toString(end));
+		int[] end = subArray(nums, nums.length - len, nums.length);	
+		return Arrays.equals(beg, end); */
 		
-		return Arrays.equals(beg, end);
+		// version 2
+		for (int i = 0; i < len; i++) {
+//			System.out.println("Algus " + nums[i]);
+//			System.out.println("Lıpust " + nums[nums.length-len+i]);
+			if (nums[i] != nums[nums.length-len+i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private int[] subArray(int[] array, int beg, int end) {
